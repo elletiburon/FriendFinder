e7ae428ea1d621dda51bd725d27ca579118830be
elletiburon <elleharrer@gmail.com>
2019-08-14 15:37:36 -0500

    friend answers set modal STILL not working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,106 +1,104 @@
-// Array that holds data objects, a list of all friends and applicable data
 
-// Might need to change the datatype of answers to string
 module.exports = [
 	{
 		name: "Kazuchika Okada",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/okada_20190125_2-352x528.png",
-		answers: [3, 1, 1, 1, 5, 4, 1, 4, 4, 5]
+		answers: [5, 3, 2, 4, 1, 1, 1, 3, 4, 2]
 	},
 	{
 		name: "Tetsuya Naito",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/naito_20190618_belt_suit_5-227x340.png",
-		answers: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
+		answers: [1, 4, 5, 5, 1, 3, 2, 5, 5, 1]
 	},
 	{
 		name: "Hiroshi Tanahashi",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/tanahashi_20190125_6-352x528.png",
-		answers: [4, 1, 2, 1, 5, 5, 1, 5, 5, 5]
+		answers: [2, 4, 3, 5, 1, 1, 5, 3, 5, 5]
 	},
 	{
 		name: "Togi Makabe",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/makabe_20190202_belt_1-227x340.png",
-		answers: [5, 1, 4, 1, 5, 5, 1, 5, 5, 5]
+		answers: [1, 4, 4, 1, 1, 3, 4, 5, 5, 5]
 	},
 	{
 		name: "Jushin Thunder Liger",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/Liger_20190329_2.png",
-		answers: [5, 1, 3, 4, 5, 5, 1, 4, 3, 5]
+		answers: [1, 4, 3, 5, 1, 5, 5, 5, 3, 1]
 	},
 	{
 		name: "Toru Yano",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/yano_20190616_3.png",
-		answers: [4, 2, 4, 4, 5, 4, 1, 5, 5, 5]
+		answers: [4, 5, 2, 2, 1, 3, 5, 5, 2, 1]
 	},
 	{
 		name: "Zack Sabre Jr",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/0007_%E3%83%AC%E3%82%A4%E3%83%A4%E3%83%BC-7.png",
-		answers: [4, 1, 1, 1, 3, 1, 1, 5, 2, 5]
+		answers: [1, 2, 4, 4, 5, 5, 1, 1, 2, 5]
 	},
 	{
 		name: "Kota Ibushi",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/03/ibushi_20180827_1.png",
-		answers: [5, 1, 2, 5, 5, 5, 1, 3, 4, 5]
+		answers: [1, 2, 5, 3, 1, 3, 1, 5, 5, 1]
 	},
 	{
 		name: "Ryusuke Taguchi",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/taguchi_20190731_3.png",
-		answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
+		answers: [1, 5, 1, 5, 1, 3, 5, 5, 2, 1]
 	},
 	{
 		name: "Minoru Suzuki",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/suzuki_20190221_3-1.png",
-		answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
+		answers: [1, 1, 5, 1, 1, 2, 1, 1, 5, 5]
 	},
 	{
 		name: "EVIL",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/EVIL_20190213_1.png",
-		answers: [5, 3, 5, 5, 5, 5, 1, 4, 4, 5]
+		answers: [1, 1, 5, 5, 1, 3, 1, 5, 5, 3]
 	},
 	{
 		name: "Dalton Castle",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/DaltonCASTLE.png",
-		answers: [5, 1, 4, 1, 5, 1, 1, 4, 2, 5]
+		answers: [4, 5, 2, 5, 5, 5, 5, 1, 4, 1]
 	},
 	{
 		name: "Hiromu Takahashi",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/06/0000s_0012_-9.png",
-		answers: [3, 1, 4, 3, 5, 5, 1, 4, 3, 5]
+		answers: [1, 4, 5, 4, 1, 5, 3, 5, 5, 1]
 	},
 	{
 		name: "Jay White",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/jay_20190719_2.png",
-		answers: [5, 1, 5, 1, 5, 1, 1, 2, 2, 1]
+		answers: [3, 1, 5, 4, 5, 1, 1, 1, 5, 1]
 	},
 	{
 		name: "Juice Robinson",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/juice_20190719_5-1.png",
-		answers: [5, 4, 5, 3, 5, 3, 4, 4, 5, 5]
+		answers: [2, 5, 1, 4, 5, 3, 5, 4, 2, 1]
 	},
 	{
 		name: "SANADA",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/SANADA_20190504_5-1.png",
-		answers: [3, 1, 3, 5, 2, 1, 1, 5, 5, 5]
+		answers: [3, 1, 5, 5, 1, 1, 2, 5, 5, 4]
 	},
 	{
 		name: "Taichi",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/taichi_20181008_belt_1.png",
-		answers: [3, 1, 1, 5, 5, 5, 1, 5, 5, 5]
+		answers: [4, 2, 5, 3, 1, 3, 4, 4, 5, 1]
 	},
 	{
 		name: "Tama Tonga",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/tama_20181213_3.png",
-		answers: [5, 3, 4, 3, 5, 5, 1, 5, 4, 5]
+		answers: [5, 3, 5, 4, 5, 2, 5, 5, 5, 2]
 	},
 	{
 		name: "Takagi Shingo",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2018/10/TakagiShingo_20190617_4.png",
-		answers: [5, 5, 5, 1, 5, 1, 1, 3, 3, 3]
+		answers: [3, 1, 5, 3, 1, 4, 2, 1, 3, 3]
 	},
 	{
 		name: "Tiger Mask",
 		photo: "https://www.njpw1972.com/wp-content/uploads/2017/02/tigermask_20190523_3.png",
-		answers: [4, 5, 4, 1, 5, 3, 2, 3, 4, 5]
+		answers: [1, 4, 3, 5, 1, 5, 3, 3, 4, 1]
 	}
 
 ];
