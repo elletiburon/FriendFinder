5c2369c5cfcaefeee2b6b42dd8fbd79f4d671ca5
elletiburon <elleharrer@gmail.com>
2019-08-13 22:29:46 -0500

    server set up

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+
+
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 
-var app = express();
-var PORT = 2500;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
