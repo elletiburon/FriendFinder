5cacda16506b671d2e05d31a58e7071334d7c46a
elletiburon <elleharrer@gmail.com>
2019-08-20 15:44:53 -0500

    modal still not working

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
-require("./app/routing/htmlRoutes.js")(app);
-require("./app/routing/apiRoutes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
+require("./app/routes/apiRoutes.js")(app);
 
 
 app.listen(PORT, function() {
